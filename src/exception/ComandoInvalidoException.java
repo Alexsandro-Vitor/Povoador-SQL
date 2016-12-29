b2bad3556ea1bf1272facf375c8624ac95a778a4
Alexsandro-Vitor <avsc@cin.ufpe.br>
2016-12-29 19:48:35 -0300

    Exceção ComandoInvalidoException renomeada

---
+++ src/exception/ComandoInvalidoException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class ComandoInvalidoException extends Exception {
+	public ComandoInvalidoException(String comando) {
+		super("O comando " + comando + " não foi reconhecido");
+	}
+}
