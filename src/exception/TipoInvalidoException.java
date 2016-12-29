cad4b06f5f1c04c7cce3ed87dc67ecd78e52d6d2
Alexsandro-Vitor <avsc@cin.ufpe.br>
2016-12-29 19:49:53 -0300

    Remoção de versão antiga da exceção

--- src/exception/TipoInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class TipoInvalidoException extends Exception {
-	public TipoInvalidoException(String tipo) {
-		super("O tipo " + tipo + " não foi reconhecido");
-	}
-}
