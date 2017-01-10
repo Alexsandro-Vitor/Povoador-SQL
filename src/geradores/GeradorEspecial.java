ea7f057484b8ac0a2dd066ecef5723ee07ec7d2d
Alexsandro-Vitor <avsc@cin.ufpe.br>
2017-01-10 14:54:48 -0300

    Tratamento de entrada do GeradorEspecial

--- src/geradores/GeradorEspecial.java
+++ src/geradores/GeradorEspecial.java
@@ -1,14 +1,25 @@
 package geradores;
 
+import exception.ComandoInvalidoException;
+
 public class GeradorEspecial extends GeradorAbstrato {
 	private String comando;
 	
-	public GeradorEspecial(String comando) {
-		this.comando = comando.substring(1, comando.length()-1);
+	public static boolean checarComando(String entrada) {
+		return (entrada.startsWith("{") && entrada.endsWith("}"));
 	}
 	
-	public String gerar() {
+	public GeradorEspecial(String comando) {
+		this.comando = comando.substring(1, comando.length() - 1);
+	}
+	
+	public String gerar() throws ComandoInvalidoException {
+		validar();
 		String[] saidas = comando.split(",");
 		return MetodosGerador.escolhaAleatoria(saidas);
+	}
+	
+	private void validar() throws ComandoInvalidoException {
+		if (comando.contains("{") || comando.contains("}")) throw new ComandoInvalidoException("{" + comando + "}");
 	}
 }
