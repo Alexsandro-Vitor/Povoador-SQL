f22ab0378c3afb18f4073b5591189dac8512da90
Alexsandro-Vitor <avsc@cin.ufpe.br>
2017-02-14 15:55:23 -0300

    Ajuste no comando CHAVE_INT e CHAVE_VARCHAR
    Agora ambos os comandos recebem um atributo determinando o valor da primeira chave

--- src/geradores/GeradorChave.java
+++ src/geradores/GeradorChave.java
@@ -2,33 +2,43 @@ package geradores;
 
 import exception.ChavesDemaisException;
 import exception.ComandoInvalidoException;
+import exception.NumeroInvalidoException;
+import exception.QtdParametrosInvalidaException;
 import povoamento.PovoamentoVariaveis;
 
 public class GeradorChave extends GeradorAbstrato {
 	private String comando;
 	private PovoamentoVariaveis variaveis;
-	
+
 	public static final String nomeComando = "CHAVE";
 	public static final String inteiro = "_INT";
 	public static final String varchar = "_VARCHAR";
-	
+
 	public static boolean checarComando(String entrada) {
-		return entrada.toUpperCase().startsWith(nomeComando);
+		return entrada.toUpperCase().startsWith(nomeComando) && entrada.endsWith(")");
 	}
-	
+
 	public GeradorChave(String comando, PovoamentoVariaveis variaveis) {
 		this.comando = comando;
 		this.variaveis = variaveis;
 	}
-	
-	public String gerar() throws ComandoInvalidoException, ChavesDemaisException {
+
+	public String gerar() throws ComandoInvalidoException, ChavesDemaisException, QtdParametrosInvalidaException, NumeroInvalidoException {
 		if (variaveis.existeChave) throw new ChavesDemaisException();
 		else {
 			variaveis.existeChave = true;
 			String tipoChave = comando.substring(nomeComando.length());
-			if (tipoChave.equalsIgnoreCase(varchar)) return "'" + variaveis.chave + "'";
-			else if (tipoChave.equalsIgnoreCase(inteiro)) return variaveis.chave;
-			else throw new ComandoInvalidoException(comando);
+			try {
+				if (tipoChave.toUpperCase().startsWith(varchar)) {
+					return MetodosGerador.varchar("" + (Integer.parseInt(MetodosGerador.validarSintaxe(tipoChave, varchar.length(), 1)[0])
+							+ Integer.parseInt(variaveis.chave)));
+				} else if (tipoChave.toUpperCase().startsWith(inteiro)) {
+					return "" + (Integer.parseInt(MetodosGerador.validarSintaxe(tipoChave, inteiro.length(), 1)[0])
+							+ Integer.parseInt(variaveis.chave));
+				} else throw new ComandoInvalidoException(comando);
+			} catch (NumberFormatException e) {
+				throw new NumeroInvalidoException(MetodosGerador.validarSintaxe(tipoChave, varchar.length(), 1)[0], tipoChave);
+			}
 		}
 	}
 }
