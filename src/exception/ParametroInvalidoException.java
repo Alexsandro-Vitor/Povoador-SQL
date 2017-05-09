ea6c648e667a31707887ce1fab9aa70d17797516
Alexsandro-Vitor <avsc@cin.ufpe.br>
2017-05-09 20:58:34 -0300

    Delete ParametroInvalidoException.java

--- src/exception/ParametroInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class ParametroInvalidoException extends Exception {
-	public ParametroInvalidoException(int parametro, String comando, int valorMinimo) {
-		super("O valor em " + parametro + " em " + comando + " deve ser maior que " + valorMinimo);
-	}
-}
