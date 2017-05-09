f4a659978bc53d615b0653285d4fa0e782193830
Alexsandro-Vitor <avsc@cin.ufpe.br>
2017-05-09 20:59:00 -0300

    Delete QtdParametrosInvalidaException.java

--- src/exception/QtdParametrosInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class QtdParametrosInvalidaException extends Exception {
-	public QtdParametrosInvalidaException(String comando, int parametros) {
-		super("A quantidade de parametros em "+comando+" deve ser exatamente "+parametros);
-	}
-}
