36112d8700eee89bd5de11cd1f565b567c004d3f
Alexsandro-Vitor <avsc@cin.ufpe.br>
2017-05-09 20:59:26 -0300

    Delete QtdSaidasInvalidaException.java

--- src/exception/QtdSaidasInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class QtdSaidasInvalidaException extends Exception {
-	public QtdSaidasInvalidaException() {
-		super("A quantidade de saídas deve ser maior que 0");
-	}
-}
